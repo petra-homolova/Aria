1838db1271b504c9e68ec1504afb6790a82a51cd
petra-homolova <petra.homolova26@gmail.com>
2024-08-26 16:51:53 +0200

    Aria

---
+++ header.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title><?php bloginfo("name") ?></title>
+    <?php wp_head() ?>
+  </head>
+  <body>
+    <div class="header">
+      <div class="nav">
+      	<?php wp_nav_menu(array("theme_location" => "header-menu-location")) ?>
+  
+     </div>
+        
+    </div>
